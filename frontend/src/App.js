6a0af803f3fe63b38aaaf6f20669b4311a9c93d0
Dustin J. Mitchell <dustin@mozilla.com>
2020-10-15 21:53:59 +0000

    pairing UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,81 @@
 import React, { Fragment } from "react";
 import Helmet from 'react-helmet';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import CogOutlineIcon from 'mdi-react/CogOutlineIcon';
 import Home from './views/Home';
 import Pairing from './views/Pairing';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
+const theme = createMuiTheme({});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    justifyContent: 'center',
+    display: 'flex',
+    background: theme.palette.primary.dark,
+  },
+  appIcon: {
+    fill: theme.palette.common.white,
+  },
+  appBar: {
+    position: 'fixed',
+    backgroundColor: theme.palette.secondary.dark,
+    zIndex: theme.zIndex.drawer + 1,
+    '& a': {
+      borderBottom: 0,
+    },
+    '& a:hover': {
+      borderBottom: 0,
+    },
+  },
+  content: {
+    backgroundColor: theme.palette.background,
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      marginTop: 64,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'end',
+  },
+}));
+
 export default function App(props) {
+  const classes = useStyles();
+
   return (
-    <Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Helmet>
         <title>Mozilla Mentorship Program</title>
       </Helmet>
       <Router>
+        <AppBar className={classes.appBar}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6">
+              Mozilla Mentoring Program
+            </Typography>
+            <a
+              href="/admin/">
+              <Tooltip placement="bottom" title="Admin Panel">
+                <IconButton>
+                  <CogOutlineIcon className={classes.appIcon} />
+                </IconButton>
+              </Tooltip>
+            </a>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route path="/pairing">
             <Pairing />
@@ -24,6 +85,6 @@ export default function App(props) {
           </Route>
         </Switch>
       </Router>
-    </Fragment>
+    </ThemeProvider>
   );
 };
