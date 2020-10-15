5a35697bfffd3eccec52413cdf450272bc28c7ee
Dustin J. Mitchell <dustin@mozilla.com>
2020-10-15 19:14:09 +0000

    switch to pairing in react, sort of

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
 import Home from './views/Home';
+import Pairing from './views/Pairing';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export default function App(props) {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/pairing">
+          <Pairing />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
