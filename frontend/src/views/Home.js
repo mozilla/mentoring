7b4b17837c2eaba3196b027d9fa7dd315e07dd87
Dustin J. Mitchell <dustin@mozilla.com>
2020-11-09 19:35:39 +0000

    move menu to the Home view

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -2,13 +2,17 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const staffMenu = (
+    <ul>
+      <li><Link to="/pairing">Pairing</Link></li>
+      <li><a href="/admin/">DB Administration Panel</a></li>
+      <li><a href="/api/">REST API Explorer</a></li>
+    </ul>
+  );
   return (
     <Fragment>
       <h1>Mozilla Mentorship Program</h1>
-      <ul>
-        <li><a href="/admin/">Administration</a></li>
-        <li><Link to="/pairing">Pairing</Link></li>
-      </ul>
+      {MENTORING_SETTINGS.user.is_staff && staffMenu}
     </Fragment>
   );
 }
