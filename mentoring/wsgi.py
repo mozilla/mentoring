c33a250c09b6e49e14fa14f697f34f5516bbe797
Dustin J. Mitchell <dustin@mozilla.com>
2021-02-16 17:32:12 +0000

    Use gunicorn to run app in production

--- mentoring/wsgi.py
+++ mentoring/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for mentoring project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mentoring.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
 application = get_wsgi_application()
