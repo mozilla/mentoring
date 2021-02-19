a45bca5e868660c613f1d86fb919b3de1d166c1f
Alberto del Barrio <adelbarrio@mozilla.com>
2021-02-19 18:06:22 +0100

    Consolidate location of env vars for prod

--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -xe
 
 export DJANGO_SETTINGS_MODULE="mentoring.settings"
+export DJANGO_CONFIGURATION="Production"
 
 python manage.py migrate
 gunicorn mentoring.wsgi:application -b 0.0.0.0:"${PORT:-8000}" --log-file -
