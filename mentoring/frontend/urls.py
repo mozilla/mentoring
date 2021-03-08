30e7dc5005fd64e61f4fe4e33c49fca110f308d6
Dustin J. Mitchell <dustin@mozilla.com>
2021-03-08 21:23:28 +0000

    Generate settings.js as a separate resource

--- mentoring/frontend/urls.py
+++ mentoring/frontend/urls.py
@@ -1,8 +1,9 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
 from . import views
 
 urlpatterns = [
+    path('settings.js', views.settings),
     # render the same view for any URL; the react router sorts it out
     re_path('.*', views.root, name='root'),
 ]
